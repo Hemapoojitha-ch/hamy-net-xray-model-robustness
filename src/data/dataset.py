cb6cb3fb30675b77e869a0af626362ee9f635da6
AnaRian7 <156986548+AnaRian7@users.noreply.github.com>
2026-04-20 10:54:57 -0500

    Implement ChestXrayDataset class for image loading

---
+++ src/data/dataset.py
@@ -0,0 +1,23 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+class ChestXrayDataset(Dataset):
+    def __init__(self, dataframe, img_dir, transform=None):
+        self.df = dataframe
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.df.iloc[idx]['path'])
+        image = Image.open(img_path).convert('RGB')
+        
+        label = self.df.iloc[idx]['label']  # 0 or 1
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, torch.tensor(label, dtype=torch.float32)
