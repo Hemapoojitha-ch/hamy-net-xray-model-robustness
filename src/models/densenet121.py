1ebd5fdc490874e4537ce4f63805bd39acee0be3
AnaRian7 <156986548+AnaRian7@users.noreply.github.com>
2026-04-20 10:52:33 -0500

    DenseNet121Binary model for binary classification

---
+++ src/models/densenet121.py
@@ -0,0 +1,18 @@
+pip install torch torchvision timm scikit-learn tqdm
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class DenseNet121Binary(nn.Module):
+    def __init__(self, pretrained=True):
+        super().__init__()
+        
+        self.model = models.densenet121(pretrained=pretrained)
+        
+        # Replace classifier
+        in_features = self.model.classifier.in_features
+        self.model.classifier = nn.Linear(in_features, 1)  # Binary output
+        
+    def forward(self, x):
+        return self.model(x)
