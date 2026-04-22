0800950f20fbe17dc64a321fbe45431fa7bdf85b
AnaRian7 <156986548+AnaRian7@users.noreply.github.com>
2026-04-21 23:49:07 -0500

    Update train.py

--- src/training/train.py
+++ src/training/train.py
@@ -1,29 +1,25 @@
-from tqdm import tqdm
 import torch
+import torch.nn as nn
+from tqdm import tqdm
 
-def train_one_epoch(model, loader, optimizer, criterion, device):
+def train_one_epoch(model, dataloader, optimizer, device):
     model.train()
-    total_loss = 0
-    num_batches = 0
+    criterion = nn.BCEWithLogitsLoss()
 
-    pbar = tqdm(loader, desc="Training")
-    for images, labels in pbar:
-        images, labels = images.to(device), labels.to(device).unsqueeze(1)
+    total_loss = 0
+
+    for images, labels in tqdm(dataloader):
+        images = images.to(device)
+        labels = labels.to(device).float().unsqueeze(1)
 
         optimizer.zero_grad()
+
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
+
         loss.backward()
-        
-        # Gradient clipping to prevent exploding gradients
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-        
         optimizer.step()
 
         total_loss += loss.item()
-        num_batches += 1
-        
-        pbar.set_postfix({'loss': total_loss / num_batches})
 
-    return total_loss / num_batches
+    return total_loss / len(dataloader)
